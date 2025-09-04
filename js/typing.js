f4436b636ccaee8a90727781047ae804f6821f7c
Mirza Shakil Ahamad <148670971+shakil13jan@users.noreply.github.com>
2025-09-04 17:37:17 +0600

    Add files via upload

---
+++ js/typing.js
@@ -0,0 +1,9 @@
+var typing = new Typed('#typing', {
+    strings: ['General Manager of Amirath Lube', 'Chemical Engineer', 'An Advisor', 'Motivational Speaker'],
+    typeSpeed: 25,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true,
+    backDelay: 2000,
+
+});
