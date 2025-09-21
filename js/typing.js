6ef77dc3f4339dd2591dc219777d9a41addb9011
Mirza Shakil Ahamad <mirzashakil.13jan@gmail.com>
2025-09-21 13:54:47 +0600

    Fianl

--- js/typing.js
+++ js/typing.js
@@ -1,9 +1,21 @@
 var typing = new Typed('#typing', {
-    strings: ['General Manager of Amirath Lube', 'Chemical Engineer', 'An Advisor', 'Motivational Speaker'],
-    typeSpeed: 25,
-    backSpeed: 0,
+    strings: ['General Manager of Amirath Lube', 'Oil Savant | Expert in Lubricant and Condition Monitoring'],
+    typeSpeed: 50,
+    backSpeed: 30,
     fadeOut: true,
     loop: true,
     backDelay: 2000,
 
 });
+
+// var typing = new Typed('#typing', {
+//     strings: [
+//         'General Manager of Amirath Lube',
+//         'Oil Savant | Expert in Lubricant and Condition Monitoring'
+//     ],
+//     typeSpeed: 50,
+//     backSpeed: 30,   // backspacing speed
+//     loop: true,
+//     backDelay: 2000, // wait before deleting
+//     // fadeOut: true, // চাইলে এই লাইন বাদ দিন
+// });
